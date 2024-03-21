5f723a21332c719086708c37eb71077eaf4f63e9
Daniel Diliberto <daniel.diliberto@hotmail.com>
2024-03-21 09:40:13 -0400

    Ramp created with lines

--- FrenzyPenguins/src/main/java/org/BobBuilders/FrenzyPenguins/CustomEntityFactory.java
+++ FrenzyPenguins/src/main/java/org/BobBuilders/FrenzyPenguins/CustomEntityFactory.java
@@ -16,10 +16,10 @@ import static org.BobBuilders.FrenzyPenguins.EntityType.*;
 public class CustomEntityFactory implements EntityFactory {
 
     @Spawns("rectangle")
-    public Entity newRectangle(SpawnData data){
+    public Entity newRectangle(SpawnData data) {
         PhysicsComponent physics = new PhysicsComponent();
         physics.setBodyType(BodyType.STATIC);
-        Rectangle view = new Rectangle(300,80);
+        Rectangle view = new Rectangle(300, 80);
         view.setFill(Color.BLUEVIOLET);
         double rotation = data.get("rotation");
         Entity entity = entityBuilder()
@@ -37,40 +37,86 @@ public class CustomEntityFactory implements EntityFactory {
     }
 
     @Spawns("ramp")
-    public Entity newRamp(SpawnData data){
-        final int offset = 300;
-
+    public Entity newRamp(SpawnData data) {
+        int horizontalRampLength = 300;
+        int verticalRampLength = 400;
+        int lowerRampLength = 50;
+        int borderLength = 50;
+        final int curveOffset = 50;
         PhysicsComponent physics = new PhysicsComponent();
-        physics.setBodyType(BodyType.STATIC);
-        QuadCurve ramp = new QuadCurve();
-        ramp.setStartX(data.getX());
-        ramp.setStartY(data.getY());
-        ramp.setEndX(data.getX()+offset);
-        ramp.setEndY(data.getX()+offset);
-        ramp.setControlX(data.getX());
-        ramp.setControlY(data.getY()+offset);
-        ramp.setStroke(Color.BLACK);
-        ramp.setStrokeWidth(2);
+        MoveTo moveTo = new MoveTo(0,
+                100);
+        Path path = new Path();
+        LineTo horizontalLine = new LineTo(moveTo.getX() + horizontalRampLength, moveTo.getY());
+        QuadCurveTo curveTo = new QuadCurveTo();
+        curveTo.setControlX(horizontalLine.getX()+curveOffset);
+        curveTo.setControlY(horizontalLine.getY());
+        curveTo.setX(horizontalLine.getX()+curveOffset);
+        curveTo.setY(horizontalLine.getY()+curveOffset);
+        LineTo verticalLine = new LineTo(curveTo.getX(), curveTo.getY()+verticalRampLength);
+
+        QuadCurveTo leftCurveTo = new QuadCurveTo();
+        leftCurveTo.setControlX(verticalLine.getX());
+        leftCurveTo.setControlY(verticalLine.getY()+curveOffset);
+        leftCurveTo.setX(verticalLine.getX()+curveOffset);
+        leftCurveTo.setY(verticalLine.getY()+curveOffset);
+
+        LineTo lowerRampLine = new LineTo(leftCurveTo.getX() + lowerRampLength, leftCurveTo.getY());
+
+        QuadCurveTo rightCurveTo = new QuadCurveTo();
+        rightCurveTo.setControlX(lowerRampLine.getX()+curveOffset);
+        rightCurveTo.setControlY(lowerRampLine.getY());
+        rightCurveTo.setX(lowerRampLine.getX()+curveOffset);
+        rightCurveTo.setY(lowerRampLine.getY()-curveOffset);
+
+        LineTo rightBorder = new LineTo(rightCurveTo.getX(), rightCurveTo.getY()+curveOffset+borderLength);
+        LineTo bottomBorder = new LineTo(rightBorder.getX()-horizontalRampLength-curveOffset*3-borderLength, rightBorder.getY());
+        LineTo leftBorder = new LineTo(bottomBorder.getX(), bottomBorder.getY()-curveOffset*2-borderLength-verticalRampLength);
+
+        path.getElements().addAll(moveTo, horizontalLine,curveTo,verticalLine,leftCurveTo,
+                lowerRampLine,rightCurveTo,rightBorder,bottomBorder,leftBorder);
+        path.setFill(Color.RED);
+        path.setStroke(Color.RED);
         Entity entity = entityBuilder()
-                .from(data)
                 .type(GROUND)
-                .view(ramp)
+                .view(path)
                 .collidable()
                 .with(physics)
                 .build();
+
+//        final int offset = 300;
+//
+//        PhysicsComponent physics = new PhysicsComponent();
+//        physics.setBodyType(BodyType.STATIC);
+//        QuadCurve ramp = new QuadCurve();
+//        ramp.setStartX(data.getX());
+//        ramp.setStartY(data.getY());
+//        ramp.setEndX(data.getX()+offset);
+//        ramp.setEndY(data.getX()+offset);
+//        ramp.setControlX(data.getX());
+//        ramp.setControlY(data.getY()+offset);
+//        ramp.setStroke(Color.BLACK);
+//        ramp.setStrokeWidth(2);
+//        Entity entity = entityBuilder()
+//                .from(data)
+//                .type(GROUND)
+//                .view(ramp)
+//                .collidable()
+//                .with(physics)
+//                .build();
         FixtureDef fix = new FixtureDef();
         fix.setDensity(0.1f);
         physics.setFixtureDef(fix);
         return entity;
     }
 
-    
+
     @Spawns("penguin")
-    public Entity newPenguin(SpawnData data){
+    public Entity newPenguin(SpawnData data) {
         PhysicsComponent physics = new PhysicsComponent();
         physics.setBodyType(BodyType.DYNAMIC);
         physics.setFixtureDef(new FixtureDef().density(0.1f));
-        Rectangle view = new Rectangle(30,30,Color.BLACK);
+        Rectangle view = new Rectangle(30, 30, Color.BLACK);
         return entityBuilder(data)
                 .type(PENGUIN)
                 .viewWithBBox(view)
@@ -81,10 +127,10 @@ public class CustomEntityFactory implements EntityFactory {
 
 
     @Spawns("begin")
-    public Entity newBegin(SpawnData data){
+    public Entity newBegin(SpawnData data) {
         PhysicsComponent physics = new PhysicsComponent();
         physics.setBodyType(BodyType.STATIC);
-        Rectangle view = new Rectangle(200,500);
+        Rectangle view = new Rectangle(200, 500);
         view.setFill(Color.BLUEVIOLET);
         Entity entity = entityBuilder()
                 .from(data)
